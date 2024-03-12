7da4cfa42b8f8b886bf787efd602e620b0933a18
koryuun <koryuun@gmail.com>
2024-03-12 08:27:04 +0300

    Кнопка обмена переделана на SVG

--- src/lib/GamePanel.svelte
+++ src/lib/GamePanel.svelte
@@ -4,8 +4,7 @@
     import { rightToLeft } from './stores'
 
     export let wordsLeft
-    let wordsTotal = "?"
-    
+
     const dispatch = createEventDispatcher()
     
 </script>
@@ -19,9 +18,29 @@
     </div>
         
     <button on:click={ ()=> dispatch("swapLeftRight") }>
-        {$rightToLeft ? "←": "→"}
+        <!-- {$rightToLeft ? "←": "→"} -->
+        <div style="transform: rotateY({$rightToLeft ? "0.5turn": "0turn"});">            
+            <svg            
+                viewBox="0 0 24 24" fill="none" 
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
+                class="lucide lucide-arrow-right-left">
+                <path d="m16 3 4 4-4 4"/>
+                <path d="M20 7H4"/>
+                <path d="m8 21-4-4 4-4"/>
+                <path d="M4 17h16"/>
+            </svg>
+        </div>
+
     </button>
-    <button on:click={() =>dispatch('close')}>❌</button>
+    <button on:click={() =>dispatch('close')}>        
+        <svg 
+            viewBox="0 0 24 24" fill="none" 
+            stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" 
+            class="lucide lucide-x">
+            <path d="M18 6 6 18"/>
+            <path d="m6 6 12 12"/>
+        </svg>        
+    </button>
 </div>
 
 <style>    
@@ -40,11 +59,25 @@
         border-width: calc(var(--size) * 0.05 );        
         padding: 0%;        
         height: var(--size);
-        width: var(--size);        
+        width: var(--size);   
+        /* Чтобы не устанавливалась странная высота */
+        line-height: 0;     
     }
 
     .info {
         font-size: calc(var(--base) * 0.04 );  
         color: black;
     }
+
+    .lucide-arrow-right-left, .lucide-x{
+        width: calc(var(--base) * 0.04 );
+        height: auto;
+    }
+
+    .lucide-x {
+        color: #C00000;        
+    }    
+
+
+
 </style>
